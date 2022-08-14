73dd7585e228c8f221d1b018cbc9f5f4ca9beb44
fmoraitis <fmoraitis.gmail.com>
2022-08-14 15:07:37 +0300

    match statement

--- scenes/BaseLevel.gd
+++ scenes/BaseLevel.gd
@@ -55,43 +55,59 @@ func create_player():
 func on_players_dashing_kill(dashing_direction:int, enemy:CharacterBody2D):
 	# here i init the enemy death scene
 	#and i free the enemy
-	enemy_death_scene_init(dashing_direction, enemy)
+	enemy_death_scene_create(dashing_direction, enemy)
 	enemy.queue_free()
 	
-func enemy_death_scene_init(dashing_direction:int, enemy:CharacterBody2D):	
+func enemy_death_scene_create(dashing_direction:int, enemy:CharacterBody2D):	
 	enemy_death_scene_node =  Enemy_Death_Scene.instantiate()
 	enemy_death_scene_node.global_position = enemy.global_position
 	enemy_death_scene_node.get_node("Visuals/Sprite").flip_h = enemy.get_node("AnimatedSprite").flip_h
 	enemy_death_scene_node.velocity.x = player_instance.velocity.x
-#	enemy_death_scene_node.velocity.y = -100	
 	# with this  i ensure that the death animation complies with the direction of the  dasing speed
 	enemy_death_scene_node.get_node("Visuals").scale.x =  dashing_direction
 	call_deferred("add_child",enemy_death_scene_node)
 	
 	
 func on_player_died(enemy_direction,type_of_enemy:ENEMY_IS):
-	player_death_scene_init(enemy_direction,type_of_enemy)
+	player_death_scene_create(enemy_direction,type_of_enemy)
 	player_instance.queue_free()
 	await get_tree().create_timer(2.5).timeout
 	add_child(level_game_over_screen.instantiate())
 
-func player_death_scene_init(enemy_type_or_direction:int, type_of_enemy:ENEMY_IS):
+func player_death_scene_create(enemy_type_or_direction:int, type_of_enemy:ENEMY_IS):
 	# 2 is a flying enemy, 0 is a spike , -1 or 1 is a regular enemy
 	player_death_scene_node =  Player_Death_Scene.instantiate()
 	player_death_scene_node.global_position = player_instance.global_position
 	player_death_scene_node.get_node("Visuals/AnimatedSprite").flip_h = player_instance.get_node("AnimatedSprite").flip_h
-	if type_of_enemy == ENEMY_IS.A_SPIKE:
-		player_death_scene_node.velocity.x = 0
-		player_death_scene_node.velocity.y = -100
-	if type_of_enemy == ENEMY_IS.FLYING:
-		player_death_scene_node.velocity = -1 * player_instance.velocity	
-	if  type_of_enemy == ENEMY_IS.ON_GROUND:
-		player_death_scene_node.velocity.x = -1*  player_instance.velocity.x
-		player_death_scene_node.velocity.y = -100	
+	
+	match typeof( type_of_enemy):
+		ENEMY_IS.A_SPIKE:
+			player_death_scene_node.velocity.x = 0
+			player_death_scene_node.velocity.y = -100
+		ENEMY_IS.FLYING:
+			player_death_scene_node.velocity = -1 * player_instance.velocity
+		ENEMY_IS.ON_GROUND:
+			player_death_scene_node.velocity.x = -1*  player_instance.velocity.x
+			player_death_scene_node.velocity.y = -100	
 		# with this  i ensure that the death animation complies with the direction of the enemy
-		player_death_scene_node.get_node("Visuals").scale.x =  enemy_type_or_direction
-#	player_death_instance.get_node("CollisionShape2D").disabled=true
+			player_death_scene_node.get_node("Visuals").scale.x =  enemy_type_or_direction
+	
 	call_deferred("add_child",player_death_scene_node)
+	
+	
+	
+#	if type_of_enemy == ENEMY_IS.A_SPIKE:
+#		player_death_scene_node.velocity.x = 0
+#		player_death_scene_node.velocity.y = -100
+#	if type_of_enemy == ENEMY_IS.FLYING:
+#		player_death_scene_node.velocity = -1 * player_instance.velocity	
+#	if  type_of_enemy == ENEMY_IS.ON_GROUND:
+#		player_death_scene_node.velocity.x = -1*  player_instance.velocity.x
+#		player_death_scene_node.velocity.y = -100	
+#		# with this  i ensure that the death animation complies with the direction of the enemy
+#		player_death_scene_node.get_node("Visuals").scale.x =  enemy_type_or_direction
+##	player_death_instance.get_node("CollisionShape2D").disabled=true
+#	call_deferred("add_child",player_death_scene_node)
 
 func on_won_level():
 #	print("won")
