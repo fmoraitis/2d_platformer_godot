eb38bacb5e331d2260106595e2bf94d869ec5e38
fmoraitis <fmoraitis.gmail.com>
2022-08-26 18:57:33 +0300

    lag while exiting fixed

--- scenes/coin.gd
+++ scenes/coin.gd
@@ -3,13 +3,19 @@ extends Node2D
 class_name Coin
 
 
-func _on_Area2D_area_entered(_area):
-	$AnimationPlayer.play("picked")
-#	$AnimationPlayer.play("free_coin")
+#func _on_Area2D_area_entered(_area):
+#	$AnimationPlayer.play("picked")
+##	$AnimationPlayer.play("free_coin")
+#	var base_level= get_tree().get_nodes_in_group("base_level")[0]
+#	base_level.coins_collected()
+#	queue_free()
+##	call_deferred("queue_free")
+#	#area.get_parent().scale *= 1.1 
+#	#print("picked")
+#	#queue_free()
+
+
+func _on_coin_picked_up_area_area_entered(area):
 	var base_level= get_tree().get_nodes_in_group("base_level")[0]
 	base_level.coins_collected()
-	queue_free()
-#	call_deferred("queue_free")
-	#area.get_parent().scale *= 1.1 
-	#print("picked")
-	#queue_free()
+	$AnimationPlayer.play("picked")
